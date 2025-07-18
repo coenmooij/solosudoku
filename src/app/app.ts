a1f547d67feb3c2a20f13fe73895cba5be1f1039
Coen Mooij <coenmooij@gmail.com>
2025-07-18 15:14:42 +0200

    add favicon, change prefix, add homepage

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ss-root',
   imports: [RouterOutlet],
   templateUrl: 'app.html',
 })
-export class App {}
+export class App {
+}
